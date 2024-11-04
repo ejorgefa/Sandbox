eb6893e152858080d0f61fe8a8f35802eeac87d4
Offlinetech <eduardofaria.mail@sapo.pt>
2024-11-04 11:32:41 +0000

    Dev branch

--- terraform/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  environment = var.stage
-  # client_application_name = "${var.client_name}-${var.application_name}"
-  # storage_account_name = "st${var.client_name}${var.application_name}${local.environment}"
-}
--- terraform/tfvars.tf
+++
@@ -1,4 +0,0 @@
-variable "NEW_RG" {
-  type        = string
-  description = "Conditional to see if we are going to create a new RG or not" 
-}
