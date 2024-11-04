ce0ecf6591d8f3cfd6d71bf218593b1028d01a40
Offlinetech <eduardofaria.mail@sapo.pt>
2024-11-04 12:08:09 +0000

    Script test

--- terraform/modules/container-apps-environment/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "container-apps-environment-id" {
-  value       = azurerm_container_app_environment.application.id
-  description = "The ID of the container apps environment."
-}
--- terraform/modules/storage-account/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  value       = azurerm_storage_account.tfstate.name
-  description = "The "
-}
-
-output "key" {
-  value       = azurerm_storage_account.tfstate.primary_access_key
-  description = "The "
-}
